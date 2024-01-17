4a7decb2e84b7aa650dc217e89f4136522719d92
Arun Kumar R <aruntutter19@gmail.com>
2024-01-17 14:25:06 +0530

    event lister - toggle added to each FAQ section

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const accordians = document.querySelectorAll(".accordian");
+
+accordians.forEach((accordian) => {
+  const icon = accordian.querySelector(".fa-caret-down");
+  const answer = accordian.querySelector(".answer");
+
+  accordian.addEventListener("click", () => {
+    icon.classList.toggle("active");
+    answer.classList.toggle("active");
+  });
+});
